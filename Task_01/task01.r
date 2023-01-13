fff58c36ed6cfb76ce0ea7cb24e2bd9bfe9afb55
EvelynnPatterson <ekp0013@mix.wvu.edu>
2023-01-13 10:47:42 -0500

    Evelyn Patterson Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,31 @@
-install.packages ("swirl")
+#install.packages ("swirl")
 library(swirl)
+swirl()
+Evelyn
+1
+1
+1
+1
+5+7
+x <- 5 +7
+x
+x-3
+y <- x -3
+y
+z <- c(1.1,9,3.14)
+?c
+z
+(z)(555)(z)
+ c(z, 555, z)
+ z*2+1000
+ my_sqrt <- sqrt(z-1)
+ 1
+ my_sqrt
+ my_div <- z/my_sqrt
+ 1
+ my_div
+ c(1,2,3,4)+ c(0,10)
+ c(1,2,3,4)+c(0,10,100)
+ my_div
+ 1
+ 
