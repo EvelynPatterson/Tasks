5e848f03c0b1320a9705cda0417b6b8ef95cf9a9
EvelynnPatterson <ekp0013@mix.wvu.edu>
2023-01-22 13:34:46 -0500

    Evelyn Patterson Task 02 upload a

--- Task_02/Untitled.R
+++ Task_02/Untitled.R
@@ -30,4 +30,48 @@ head(Data1)
 beren2 <- Data1
 beren3 <- beren2[order(beren2$age) ,]
 write.csv(beren3, 'beren_new.csv', quote=F, row.names=FALSE)
-
+Feeds <- which(beren3$event == "bottle")
+avgMilk <- mean(beren3$value[Feeds])
+avgFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], mean)
+varFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], var)
+totalFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], sum)
+numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length)
+cor(beren3$value[Feeds], beren3$age[Feeds])
+cor.test(beren3$value[Feeds], beren3$age[Feeds])
+berenCor <- cor.test(beren3$value[Feeds], beren3$age[Feeds])
+summary(berenCor)
+berenANOVA <- aov(beren3$value[Feeds]~beren3$caregiver[Feeds])
+boxplot(beren3$value[Feeds]~beren3$caregiver[Feeds], xlab="who gave the bottle", ylab="amount of milk consumed (oz)")
+par(las=1, mar=c(5,5,1,1), mgp=c(2,0.5,0), tck=-0.01)
+plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed), lty=2, col='red')
+pdf("r02b-totalMilkByDay.pdf", height=4, width=4)
+par(las=1, mar=c(5,5,1,1), mgp=c(2,0.5,0), tck=-0.01)
+plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed), lty=2, col='red')
+dev.off()
+source('plotFxn02b.R')
+pdf("r02b-cumulativeMilkByTime.pdf")
+length(Data2)
+nrow(Data2)
+ncol(Data2)
+colnames(Data2)
+head(Data2)
+Data2[1,]
+Data2[2, ]
+Data2[1:3,]
+Data2[1:3,4]
+Data2[1:5, 1:3]
+Mass <- which(Data2[,9] == 'trait_mass')
+cyrusMilk <- Data2[Mass, ]
+head(cyrusMilk)
+nrow(Data2)
+Mass <- which(Data2[,'event'] == 'trait_mass')
+Mass <- which(Data2$event=='trait_mass')
+dayID <- apply(Data2, 1, function(x) paste(x[1:3], collapse='-'))
+dateID <- sapply(dayID, as.Date, format = "%Y-%m-%d", origin="2022-04-12")
+Data2$age <- dateID-dateID[which(Data2$event == 'birth')]
+head(Data2)
+cyrus2 <- Data2
+cyrus3 <- cyrus2[order(cyrus2$age), ]
+write.csv(cyrus3, 'cyrus_new.csv', quote=F, row.names=FALSE)
