ed270b3700d4340f9466622304695de8b061a7fc
EvelynnPatterson <ekp0013@mix.wvu.edu>
2023-02-02 13:06:50 -0500

    Evelyn Patterson Task 04

---
+++ Task_04/task04.r
@@ -0,0 +1,22 @@
+setwd('~/Desktop/Evolution/Tasks/Task_04')
+source("fxn05.R")
+Pop1 <- simPop(Popsize = 50, nGenerations = 100, initial_p = 0.5, h = 1, s = 0)
+
+pdf('Pop1.pdf')
+plot(1:nrow(Pop1), Pop1 [,1], ylim=c(0,1), type = "l", xlab="generation", ylab="allele freq.", lwd=2)
+lines(1:nrow(Pop1), Pop1[,2], lwd=2, col='red')
+legend("topleft", legend=c("a", "b"), col=c("black", "red"), lwd=2, bty="n")
+dev.off()
+
+pdf('Graph2.pdf')
+plotFit(nruns=10, n=50, ngens=100, init_p=0.5, h=1, s=0)
+dev.off()
+
+Expectation <- c(10,10,10,10)
+Observed <- c(15,15,5,5)
+Chisq <- sum(((Expectation-Observed)^2)/Expectation)
+pdf('barplot.pdf')
+barplot(rbind(Expectation,Observed), beside =T, main = bquote(chi^2 ~ "=" ~ . (Chisq)), legend.text=c("expected", "observed"))
+dev.off()
+Expectation <- c(10,10,10,10)
+Observerd <- c(5,0,0,35)
